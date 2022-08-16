0c879c0d46c56d74eaaec1d843f6c8982007336b
asadkhalidd <asadkhalid421@gmail.com>
2022-08-16 17:25:32 +0500

    Delete Item

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, CreateView, TaskCreate
+from .views import TaskList, TaskDetail, CreateView, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>', TaskDetail.as_view(), name='task'),
     path('create-task', TaskCreate.as_view(), name='task-create'),
+    path('update-task/<int:pk>', TaskUpdate.as_view(), name='task-update'),
+    path('delete-task/<int:pk>', TaskDelete.as_view(), name='task-delete'),
 ]
